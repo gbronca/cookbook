91fac16bab88eff28752b3dd45960b0b86272a22
gbronca <gbronca@gmail.com>
2019-08-20 19:44:33 +0100

    updated app.run call

--- app.py
+++ app.py
@@ -373,6 +373,6 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP'),
-            port=int(os.getenv('PORT')),
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
             debug=True)
